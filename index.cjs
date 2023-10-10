e4796e7df45900c550d23bc182ee77a04b6b5828
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-10-10 08:37:25 -0500

    fix: fix download url function to include tag

--- index.cjs
+++ index.cjs
@@ -9,7 +9,14 @@ const ERA_TEST_NODE_RELEASE_TAG = getInput('releaseTag') || 'latest';
 const ERA_TEST_NODE_ARCH = getInput('target') || 'x86_64-unknown-linux-gnu';
 
 async function getDownloadUrl() {
-  const response = await fetch(`https://api.github.com/repos/matter-labs/era-test-node/releases/tags/${ERA_TEST_NODE_RELEASE_TAG}`);
+  let apiUrl;
+  if (ERA_TEST_NODE_RELEASE_TAG === 'latest') {
+    apiUrl = 'https://api.github.com/repos/matter-labs/era-test-node/releases/latest';
+  } else {
+    apiUrl = `https://api.github.com/repos/matter-labs/era-test-node/releases/tags/${ERA_TEST_NODE_RELEASE_TAG}`;
+  }
+
+  const response = await fetch(apiUrl);
   if (!response.ok) {
     throw new Error(`Failed to fetch release info for tag ${ERA_TEST_NODE_RELEASE_TAG}. HTTP Status: ${response.status}`);
   }
